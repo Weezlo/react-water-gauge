3d1baf23c35ac00a30e70ac4aec4cc34baf0295b
Anis NOUIRA <weezlo.an@gmail.com>
2016-09-25 23:08:14 +0200

    Clean up + css added

--- dist/WaterGauge.js
+++ dist/WaterGauge.js
@@ -53,28 +53,31 @@ var WaterGauge =
 /* 1 */
 /***/ function(module, exports) {
 
-	'use strict';
+	"use strict";
 
 	var WaterGauge = React.createClass({
-	  displayName: 'WaterGauge',
+	  displayName: "WaterGauge",
 
 	  propTypes: {
-	    size: React.PropTypes.number,
+	    minValue: React.PropTypes.number,
+	    maxValue: React.PropTypes.number,
+	    value: React.PropTypes.number,
+	    label: React.PropTypes.string,
+	    size: React.PropTypes.oneOf(["sm", "lg"]),
 	    backgroundColor: React.PropTypes.string,
 	    liquidColor: React.PropTypes.string,
-	    percentColor: React.PropTypes.string
+	    labelColor: React.PropTypes.string
 	  },
 	  getDefaultProps: function getDefaultProps() {
 	    return {
-	      size: 100,
-	      backgroundColor: 'white',
-	      liquidColor: 'blue',
-	      percentColor: 'white'
-	    };
-	  },
-	  getInitialState: function getInitialState() {
-	    return {
-	      percentComplete: 0
+	      minValue: 0,
+	      maxValue: 100,
+	      value: 0,
+	      label: "",
+	      size: "sm",
+	      backgroundColor: "white",
+	      liquidColor: "blue",
+	      labelColor: "white"
 	    };
 	  },
 	  componentDidMount: function componentDidMount() {
@@ -84,68 +87,71 @@ var WaterGauge =
 	    this.updateCss();
 	  },
 	  updateCss: function updateCss() {
-	    document.getElementById("wave").style.top = "" + (this.props.size - this.state.percentComplete * this.props.size / 100 - 5) + "px";
-	    document.getElementById("wave").style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg' width='240' height='600'%3E%3Cg transform='scale(1,0.20000000149011612) '%3E%3Cpath fill='" + this.props.liquidColor + "' d='m425.600824,8090.652986c-161.801,-1.801 -112.507645,211.167255 -271.308645,209.366255c-158.801,-1.80101 -153.908645,-167.958678 -227.308645,-168.559678c-73.4,-0.601 -145.613992,-753.851837 -163.213992,-753.851837l-3.139912,-7341.522536c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c42.399,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4l0,48l31.25926,973.48148z'/%3E%3C/g%3E%3C/svg%3E\")";
-	  },
-	  changePercentComplete: function changePercentComplete() {
-	    if (this.state.percentComplete >= 100) return;
-	    this.setState({ percentComplete: this.state.percentComplete + 5 });
-	  },
+	    var size = this.props.size === 'sm' ? 100 : 200;
+	    var percentComplete = this.props.value / (this.props.maxValue - this.props.minValue);
+	    ReactDOM.findDOMNode(this.refs.rwgWave).style.top = '' + (size - percentComplete * size - 5) + 'px';
 
+	    ReactDOM.findDOMNode(this.refs.rwgWave).style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg' width='240' height='600'%3E%3Cg transform='scale(1,0.20000000149011612) '%3E%3Cpath fill='" + this.props.liquidColor + "' d='m425.600824,8090.652986c-161.801,-1.801 -112.507645,211.167255 -271.308645,209.366255c-158.801,-1.80101 -153.908645,-167.958678 -227.308645,-168.559678c-73.4,-0.601 -145.613992,-753.851837 -163.213992,-753.851837l-3.139912,-7341.522536c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c42.399,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4l0,48l31.25926,973.48148z'/%3E%3C/g%3E%3C/svg%3E\")";
+	  },
+	  computeLabel: function computeLabel() {
+	    if (!this.props.label) return this.props.value;
+	    return this.props.label;
+	  },
 	  render: function render() {
 	    var _props = this.props;
 	    var backgroundColor = _props.backgroundColor;
 	    var liquidColor = _props.liquidColor;
-	    var percentColor = _props.percentColor;
-	    var size = _props.size;
+	    var labelColor = _props.labelColor;
 
 
-	    var percentComplete = this.state.percentComplete;
-
+	    var size = this.props.size === 'sm' ? 100 : 200;
+	    var computedLabel = this.computeLabel();
 	    return React.createElement(
-	      'div',
-	      { onClick: this.changePercentComplete.bind(this) },
+	      "div",
+	      null,
 	      React.createElement(
-	        'svg',
-	        { width: size, height: size, style: { zIndex: 5, overflow: "visible" } },
-	        React.createElement('circle', { id: 'surr-circle', r: size / 2 + 3, cx: size / 2, cy: size / 2, fill: 'none', stroke: liquidColor, strokeWidth: '2' })
+	        "svg",
+	        { id: "rwg-outer-circle", className: "rwg-svg",
+	          width: size, height: size },
+	        React.createElement("circle", { r: size / 2 + 3, cx: size / 2, cy: size / 2,
+	          fill: "none", stroke: liquidColor, strokeWidth: "2" })
 	      ),
 	      React.createElement(
-	        'div',
-	        { className: 'water-level',
+	        "div",
+	        { id: "rwg-water-level",
 	          style: {
 	            backgroundColor: backgroundColor,
 	            height: size + "px",
 	            width: size + "px"
 	          } },
 	        React.createElement(
-	          'svg',
-	          { id: 'svg', width: size, height: size },
+	          "svg",
+	          { id: "rwg-svg-mask", className: "rwg-svg",
+	            width: size, height: size },
 	          React.createElement(
-	            'defs',
+	            "defs",
 	            null,
 	            React.createElement(
-	              'mask',
-	              { id: 'hole' },
-	              React.createElement('rect', { width: '100%', height: '100%', fill: 'white' }),
-	              React.createElement('circle', { id: 'mask-circle', r: size / 2, cx: size / 2, cy: size / 2 })
+	              "mask",
+	              { id: "hole" },
+	              React.createElement("rect", { width: "100%", height: "100%", fill: "white" }),
+	              React.createElement("circle", { id: "mask-circle", r: size / 2, cx: size / 2, cy: size / 2 })
 	            )
 	          ),
-	          React.createElement('rect', { id: 'rect',
+	          React.createElement("rect", { id: "rect",
 	            width: size, height: size,
-	            x: '0', y: '0',
-	            fill: backgroundColor, mask: 'url(#hole)' })
+	            x: "0", y: "0",
+	            fill: backgroundColor, mask: "url(#hole)" })
 	        ),
-	        React.createElement('div', { id: 'wave',
+	        React.createElement("div", { id: "rwg-wave", ref: "rwgWave",
 	          style: {
 	            height: size + 30 + "px",
 	            width: size + 100 + "px"
 	          } }),
 	        React.createElement(
-	          'h1',
-	          { className: 'percentage', style: { color: percentColor } },
-	          percentComplete,
-	          '%'
+	          "h1",
+	          { id: "rwg-label", style: { color: labelColor } },
+	          computedLabel
 	        )
 	      )
 	    );
--- src/react-water-gauge.js
+++ src/react-water-gauge.js
@@ -1,21 +1,24 @@
 const WaterGauge = React.createClass({
   propTypes: {
-    size: React.PropTypes.number,
+    minValue: React.PropTypes.number,
+    maxValue: React.PropTypes.number,
+    value: React.PropTypes.number,
+    label: React.PropTypes.string,
+    size: React.PropTypes.oneOf(["sm", "lg"]),
     backgroundColor: React.PropTypes.string, 
     liquidColor: React.PropTypes.string,
-    percentColor: React.PropTypes.string 
+    labelColor: React.PropTypes.string 
   },
   getDefaultProps: function() {
     return {
-      size: 100,
-      backgroundColor: 'white', 
-      liquidColor: 'blue',
-      percentColor: 'white' 
-    };
-  },
-  getInitialState: function(){
-    return {
-      percentComplete: 0
+      minValue: 0,
+      maxValue: 100,
+      value: 0,
+      label: "",
+      size: "sm",
+      backgroundColor: "white", 
+      liquidColor: "blue",
+      labelColor: "white" 
     };
   },
   componentDidMount: function(){
@@ -24,37 +27,44 @@ const WaterGauge = React.createClass({
   componentDidUpdate: function(prevProps, prevState){
     this.updateCss();
   },
-  updateCss(){
-    document.getElementById("wave").style.top = 
-      "" + (this.props.size - this.state.percentComplete*this.props.size/100 - 5) + "px";
-    document.getElementById("wave").style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg' width='240' height='600'%3E%3Cg transform='scale(1,0.20000000149011612) '%3E%3Cpath fill='" + this.props.liquidColor + "' d='m425.600824,8090.652986c-161.801,-1.801 -112.507645,211.167255 -271.308645,209.366255c-158.801,-1.80101 -153.908645,-167.958678 -227.308645,-168.559678c-73.4,-0.601 -145.613992,-753.851837 -163.213992,-753.851837l-3.139912,-7341.522536c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c42.399,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4l0,48l31.25926,973.48148z'/%3E%3C/g%3E%3C/svg%3E\")";
+  updateCss: function (){
+    const size = this.props.size === 'sm' ? 100 : 200;
+    const percentComplete = this.props.value/(this.props.maxValue - this.props.minValue);
+    ReactDOM.findDOMNode(this.refs.rwgWave).style.top = 
+      '' + (size - percentComplete*size - 5) + 'px';
+    
+    ReactDOM.findDOMNode(this.refs.rwgWave).style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg' width='240' height='600'%3E%3Cg transform='scale(1,0.20000000149011612) '%3E%3Cpath fill='" + this.props.liquidColor + "' d='m425.600824,8090.652986c-161.801,-1.801 -112.507645,211.167255 -271.308645,209.366255c-158.801,-1.80101 -153.908645,-167.958678 -227.308645,-168.559678c-73.4,-0.601 -145.613992,-753.851837 -163.213992,-753.851837l-3.139912,-7341.522536c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c42.399,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4l0,48l31.25926,973.48148z'/%3E%3C/g%3E%3C/svg%3E\")";
+    
   },
-  changePercentComplete(){
-    if( this.state.percentComplete >= 100 ) return;
-    this.setState({percentComplete: this.state.percentComplete+5})
+  computeLabel: function (){
+    if(!this.props.label) 
+      return this.props.value;
+    return this.props.label;
   },
-  render: function(){
+  render: function (){
     const { 
       backgroundColor, 
       liquidColor,
-      percentColor,
-      size
+      labelColor
     } = this.props;
     
-    const percentComplete = this.state.percentComplete;
-
+    const size = this.props.size === 'sm' ? 100 : 200;
+    const computedLabel = this.computeLabel();
     return (
-      <div onClick={this.changePercentComplete.bind(this)}>
-        <svg width={size} height={size} style={{zIndex: 5, overflow: "visible"}}>
-          <circle id="surr-circle" r={(size/2)+3} cx={size/2} cy={size/2} fill="none" stroke={liquidColor} strokeWidth="2"/>
+      <div>
+        <svg id="rwg-outer-circle" className="rwg-svg" 
+          width={size} height={size}>
+          <circle r={(size/2)+3} cx={size/2} cy={size/2} 
+            fill="none" stroke={liquidColor} strokeWidth="2"/>
         </svg> 
-        <div className="water-level"
+        <div id="rwg-water-level"
           style={{
             backgroundColor: backgroundColor,
             height: size+"px",
             width: size+"px",
           }}>
-          <svg id="svg" width={size} height={size}>
+          <svg id="rwg-svg-mask" className="rwg-svg" 
+            width={size} height={size}>
             <defs>
               <mask id="hole">
                 <rect width="100%" height="100%" fill="white"/>
@@ -67,12 +77,14 @@ const WaterGauge = React.createClass({
               x="0" y="0" 
               fill={backgroundColor} mask="url(#hole)" />
           </svg>
-          <div id="wave" 
+          <div id="rwg-wave" ref="rwgWave"
             style={{
               height: size+30+"px",
               width: size+100+"px"
             }}/> 
-          <h1 className="percentage" style={{color: percentColor}}>{percentComplete}%</h1>
+          <h1 id="rwg-label" style={{color: labelColor}}>
+            {computedLabel}
+          </h1>
         </div>
       </div>
       
