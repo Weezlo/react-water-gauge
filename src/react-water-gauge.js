50ee853f75314ed2b0b360b18875ea7a86237d58
Anis NOUIRA <weezlo.an@gmail.com>
2016-09-25 18:48:45 +0200

    vertical level adjustment

--- src/react-water-gauge.js
+++ src/react-water-gauge.js
@@ -26,7 +26,7 @@ const WaterGauge = React.createClass({
   },
   updateCss(){
     document.getElementById("wave").style.top = 
-      "" + (this.props.size - this.state.percentComplete*this.props.size/100) + "px";
+      "" + (this.props.size - this.state.percentComplete*this.props.size/100 - 5) + "px";
     document.getElementById("wave").style.backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg' width='240' height='600'%3E%3Cg transform='scale(1,0.20000000149011612) '%3E%3Cpath fill='" + this.props.liquidColor + "' d='m425.600824,8090.652986c-161.801,-1.801 -112.507645,211.167255 -271.308645,209.366255c-158.801,-1.80101 -153.908645,-167.958678 -227.308645,-168.559678c-73.4,-0.601 -145.613992,-753.851837 -163.213992,-753.851837l-3.139912,-7341.522536c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c42.399,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4c16.8,0 25.6,-10.4 40.8,-30.4c16,-20.8 36.8,-49.6 79.2,-49.6c41.6,0 63.199,28.799 79.199,49.6c15.2,20 23.2,30.4 40.801,30.4l0,48l31.25926,973.48148z'/%3E%3C/g%3E%3C/svg%3E\")";
   },
   changePercentComplete(){
@@ -69,7 +69,7 @@ const WaterGauge = React.createClass({
           </svg>
           <div id="wave" 
             style={{
-              height: size+"px",
+              height: size+30+"px",
               width: size+100+"px"
             }}/> 
           <h1 className="percentage" style={{color: percentColor}}>{percentComplete}%</h1>
